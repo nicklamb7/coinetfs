a4ffe78e8481e149f42d52a0721f4ba84c9dfc82
Nick Lamb <nlamb@hotmail.co.uk>
2021-10-26 16:33:06 +0100

    update db migrations

--- db/migrate/20211026145429_change_user_on_articles.rb
+++
@@ -1,5 +0,0 @@
-class ChangeUserOnArticles < ActiveRecord::Migration[6.0]
-  def change
-    change_column_null :articles, :user_id, true
-  end
-end
--- db/migrate/20211026152003_remove_user_from_article.rb
+++
@@ -1,5 +0,0 @@
-class RemoveUserFromArticle < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :articles, :user_id, :bigint
-  end
-end
